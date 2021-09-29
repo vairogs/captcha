147b9f5ae125ea3bb7485ff7237111ae2a9358bc
Davis Zalitis <davis.zalitis@lattelecom.lv>
2021-09-29 16:05:33 +0300

    structure reorganization ][ captcha

---
+++ Form/Type/Honey.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace Vairogs\Captcha\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vairogs\Component\Utils\DependencyInjection\Component;
+use Vairogs\Component\Utils\Vairogs;
+use function sprintf;
+
+class Honey extends HiddenType
+{
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(defaults: [
+            'attr' => ['value' => ''],
+            'required' => false,
+            'translation_domain' => sprintf('%s_%s', Vairogs::VAIROGS, Component::CAPTCHA),
+        ]);
+    }
+}
